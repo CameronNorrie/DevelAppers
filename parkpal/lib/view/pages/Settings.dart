eae9598a7b8a58cbd5cc93b73cc39dfc1bd5e53f
Christopher Rupert <rupertc@uoguelph.ca>
2023-03-12 21:40:02 -0400

    Changes + added setting page

--- parkpal/lib/view/pages/Settings.dart
+++ parkpal/lib/view/pages/Settings.dart
@@ -1,8 +1,45 @@
 import 'package:flutter/material.dart';
 
-Widget settings(){
-  return SafeArea(child: SingleChildScrollView(
-    child:Column(
-      children: [Text("poggers in the chat")],
-    )));
+Widget settings() {
+  return SafeArea(
+      child: Column(
+    children: [
+      Container(
+          child: Row(
+        children: [
+          Expanded(
+              flex: 3,
+              child: CircleAvatar(
+                backgroundImage: AssetImage("asset/images/profile.png"),
+                radius: 40,
+              )),
+          Expanded(
+              flex: 7,
+              child: Text(
+                "John Doe",
+                style: TextStyle(fontSize: 30),
+                textAlign: TextAlign.center,
+              ))
+        ],
+      )),
+      Padding(padding: EdgeInsets.only(top: 20)),
+      ListTile(
+          title: Text("Privacy"),
+          shape: Border(top: BorderSide(), bottom: BorderSide()),
+          trailing: Icon(Icons.arrow_forward_ios)),
+      ListTile(
+          title: Text("Notifcations"),
+          shape: Border(bottom: BorderSide()),
+          trailing: Icon(Icons.arrow_forward_ios)),
+      ListTile(
+          title: Text("Password"),
+          shape: Border(bottom: BorderSide()),
+          trailing: Icon(Icons.arrow_forward_ios)),
+      ListTile(
+        title: Text("Account"),
+        shape: Border(bottom: BorderSide()),
+        trailing: Icon(Icons.arrow_forward_ios),
+      ),
+    ],
+  ));
 }
