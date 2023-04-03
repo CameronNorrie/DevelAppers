7610d8846235d295dc84a94f16a65250d6f47bd9
Farid-Hamid <faridhamid00@gmail.com>
2023-04-03 17:03:30 -0400

    Can now obtain the long and lat values of a pin

--- parkpal/lib/view/pages/myMap.dart
+++ parkpal/lib/view/pages/myMap.dart
@@ -1,47 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-
 late GoogleMapController mapController;
 
-final LatLng _center = const LatLng(43.532723980199435, -80.22618390079778);
+const LatLng _center = LatLng(43.532723980199435, -80.22618390079778);
+
+Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
 
 void _onMapCreated(GoogleMapController controller) {
   mapController = controller;
 }
-Widget mapPage(){
-  return Stack(children: [
-          GoogleMap(
-            onMapCreated: _onMapCreated, 
-            initialCameraPosition: 
-            CameraPosition(
-              target: _center, 
-              zoom:15.0
-            ),
-          ),
-          Positioned(
-            top:90,
-            right:15,
-            left:15,
-            child: Container(
-              decoration: BoxDecoration(color: Colors.white.withOpacity(0.9), borderRadius: BorderRadius.all(Radius.circular(20))),
-              child:
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        contentPadding: EdgeInsets.symmetric(horizontal: 15),
-                        hintText: "Search"
-                      ),
-                    ),
-                  ), 
-                ],
-              )
-            )
-          )
-        ],
-      );
+
+class myMap extends StatefulWidget {
+  const myMap({super.key});
+
+  @override
+  State<myMap> createState() => _myMapState();
 }
 
+class _myMapState extends State<myMap> {
+  Future _addMarkerLongPressed(LatLng latlang) async {
+    setState(() {
+      final MarkerId markerId = MarkerId("RANDOM_ID");
+      Marker marker = Marker(
+        markerId: markerId,
+        draggable: true,
+        position: latlang,
+        icon: BitmapDescriptor.defaultMarker,
+      );
+
+      markers[markerId] = marker;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(target: _center, zoom: 15.0),
+          onLongPress: (latlang) {
+            _addMarkerLongPressed(latlang);
+          },
+          markers: Set<Marker>.of(markers.values),
+        ),
+        Positioned(
+            top: 90,
+            right: 15,
+            left: 15,
+            child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.9),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 15),
+                            hintText: "Search"),
+                      ),
+                    ),
+                  ],
+                )))
+      ],
+    );
+  }
+}
